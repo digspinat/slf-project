6107ea2d6a44454003a26f22a018674299e412e5
Milan Patel <cmilan@pistalix.com>
2018-12-28 15:05:29 +0530

    Changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
-// const path = require('path');
-// const slash = require('slash');
-// const {kebabCase, uniq, get, compact, times} = require('lodash');
+const path = require('path');
+const slash = require('slash');
+const {createFilePath} = require(`gatsby-source-filesystem`)
+const {kebabCase, uniq, get, compact, times} = require('lodash');
+
 //
 // // Don't forget to update hard code values into:
 // // - `templates/blog-page.tsx:23`
@@ -13,97 +15,146 @@
 // // Slug will used for blog page path.
 // exports.onCreateNode = ({node, actions, getNode}) => {
 //   const {createNodeField} = actions;
-//   let slug;
-//   switch (node.internal.type) {
-//     case `MarkdownRemark`:
-//       const fileNode = getNode(node.parent);
-//       const [basePath, name] = fileNode.relativePath.split('/');
-//       slug = `/${basePath}/${name}/`;
-//       break;
-//   }
-//   if (slug) {
-//     createNodeField({node, name: `slug`, value: slug});
-//   }
+  // let slug;
+  // switch (node.internal.type) {
+  //   case `MarkdownRemark`:
+  //     const fileNode = getNode(node.parent);
+  //     const [basePath, name] = fileNode.relativePath.split('/');
+  //     slug = `/${basePath}/${name}/`;
+  //     break;
+  // }
+  // if (slug) {
+  //   createNodeField({node, name: `slug`, value: slug});
+  // }
 // };
 //
 // // Implement the Gatsby API `createPages`.
 // // This is called after the Gatsby bootstrap is finished
 // // so you have access to any information necessary to
 // // programatically create pages.
-// exports.createPages = ({graphql, actions}) => {
-//   const {createPage} = actions;
-//
-//   return new Promise((resolve, reject) => {
-//     const templates = ['blogPost', 'tagsPage', 'blogPage']
-//       .reduce((mem, templateName) => {
-//         return Object.assign({}, mem,
-//         {[templateName]: path.resolve(`src/templates/${kebabCase(templateName)}.tsx`)});
-//       }, {});
-//
-//     graphql(
-//       `
-//       {
-//         posts: allMarkdownRemark {
-//           edges {
-//             node {
-//               fields {
-//                 slug
-//               }
-//               frontmatter {
-//                 tags
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `
-//     ).then(result => {
-//       if (result.errors) {
-//         return reject(result.errors);
-//       }
-//       const posts = result.data.posts.edges.map(p => p.node);
-//
-//       // Create blog pages
-//       posts
-//         .filter(post => post.fields.slug.startsWith('/blog/'))
-//         .forEach(post => {
-//           createPage({
-//             path: post.fields.slug,
-//             component: slash(templates.blogPost),
-//             context: {
-//               slug: post.fields.slug
-//             }
-//           });
-//         });
-//
-//       // Create tags pages
-//       posts
-//         .reduce((mem, post) =>
-//           cleanArray(mem.concat(get(post, 'frontmatter.tags')))
-//         , [])
-//         .forEach(tag => {
-//           createPage({
-//             path: `/blog/tags/${kebabCase(tag)}/`,
-//             component: slash(templates.tagsPage),
-//             context: {
-//               tag
-//             }
-//           });
-//         });
-//
-//       // Create blog pagination
-//       const pageCount = Math.ceil(posts.length / POSTS_PER_PAGE);
-//       times(pageCount, index => {
-//         createPage({
-//           path: `/blog/page/${index + 1}/`,
-//           component: slash(templates.blogPage),
-//           context: {
-//             skip: index * POSTS_PER_PAGE
-//           }
-//         });
-//       });
-//
-//       resolve();
-//     });
-//   });
-// };
+exports.createPages = ({graphql, actions}) => {
+  const {createPage} = actions;
+
+  return new Promise((resolve, reject) => {
+    const templates = ['blogPost', 'tagsPage', 'blogPage']
+      .reduce((mem, templateName) => {
+        return Object.assign({}, mem,
+        {[templateName]: path.resolve(`src/templates/${kebabCase(templateName)}.tsx`)});
+      }, {});
+    graphql(
+      `
+      {
+        posts: allDatoCmsMenu(filter: { locale: { eq: "en"}, menu05: { eq: "Header"}}) {
+          edges {
+            node {
+              menu05
+              menu15 {
+                page05
+                page30
+              }
+              menu105{
+                page05
+                page30
+              }
+              menu60
+              {
+                page05
+                page30
+              }
+              menu110{
+                page05
+                page30
+              }
+              menu90
+              {
+                page05
+                page30
+              }
+
+            }
+          }
+        }
+      }
+    `
+    ).then(result => {
+      if (result.errors) {
+        return reject(result.errors);
+      }
+      const posts = result.data.posts.edges.map(p => p.node);
+      const blogPostTemplate = path.resolve(`src/templates/pages.js`);
+      // Create blog pages
+      posts
+        .filter(post => post.menu05.startsWith('Header'))
+        .forEach(post => {
+          if(typeof(post) == "object"){
+            if(post.menu15 != null){
+              if("menu15" in post){
+                let slug = post.menu15.page30;
+                createPage({
+                  path: `/${slug}/`,
+                  component: blogPostTemplate,
+                  context: {
+                    slug: `${slug}`,
+                    pagetitle: post.menu15.page05
+                  }
+                });
+              }
+            }
+            if(post.menu105 != null){
+              if("menu105" in post){
+                let slug = post.menu105.page30;
+                createPage({
+                  path: `/${slug}/`,
+                  component: blogPostTemplate,
+                  context: {
+                    slug: `${slug}`,
+                    pagetitle: post.menu105.page05
+                  }
+                });
+              }
+            }
+            if(post.menu60 != null){
+                if("menu60" in post){
+                  let slug = post.menu60.page30;
+                  createPage({
+                    path: `/${slug}/`,
+                    component: blogPostTemplate,
+                    context: {
+                      slug: `${slug}`,
+                      pagetitle: post.menu60.page05
+                    }
+                  });
+                }
+              }
+            if(post.menu110 != null){
+                if("menu110" in post){
+                  let slug = post.menu110.page30;
+                  createPage({
+                    path: `/${slug}/`,
+                    component: blogPostTemplate,
+                    context: {
+                      slug: `${slug}`,
+                      pagetitle: post.menu110.page05
+                    }
+                  });
+                }
+              }
+            if(post.menu90 != null){
+                if("menu90" in post){
+                  let slug = post.menu90.page30;
+                  createPage({
+                    path: `/${slug}/`,
+                    component: blogPostTemplate,
+                    context: {
+                      slug: `${slug}`,
+                      pagetitle: post.menu90.page05
+                    }
+                  });
+                }
+              }
+          }
+        });
+        resolve();
+    });
+  });
+};
